81e60bbda322be9b798e16119807dc197da62cbf
Aung Htet <akhtet@gmail.com>
2022-03-13 14:07:33 +1100

    Update build script to analyze and summarize errors

--- script/build.py
+++ script/build.py
@@ -2,7 +2,7 @@
 Run this script from the main directory.
 """
 
-import csv, sys, os, re
+import csv, sys, os, re, yaml
 
 dev_file = 'xnli-original/xnli.dev.tsv'
 test_file = 'xnli-original/xnli.test.tsv'
@@ -53,11 +53,13 @@ def build_dict():
 
 def analyze_file(fname):
     """
-    Missed sequences
-    Review Tags
-    Spelling errors / Find and replace
-    Tabs, doublequotes
+    Finds issues with the translation file:
+    - Missed sequences
+    - Review Tags
+    - Spelling errors / Find and replace
+    - Tabs, doublequotes in lines
 
+    The output dict format is
     file_name: string
         sequence number: integer
             source: string
@@ -65,7 +67,6 @@ def analyze_file(fname):
             review: boolean            
             comments: list[string]
             errors: list[string]
-
     """
 
     def is_seq_num(line):
@@ -83,6 +84,14 @@ def analyze_file(fname):
 
     def is_blank(line):
         return len(line) == 0
+
+    def is_invalid(line):
+        for token in line.split():
+            if '"' in token:
+                return True
+            if '\t' in token:
+                return True
+        return False
 
     def is_review(comments):
         for line in comments:
@@ -124,65 +133,69 @@ def analyze_file(fname):
                 state = 2
             
             elif state == 2:
-                if is_Burmese(line) or line == '<MYANMAR UNICODE TRANSLATION HERE>':
-                    blocks[seq_num]['target'] = line                                         
+                if is_Burmese(line):
+                    blocks[seq_num]['target'] = line
+                    if is_invalid(line):
+                        blocks[seq_num]['errors'].append('Invalid characters found in translation')
+                elif line == '<MYANMAR UNICODE TRANSLATION HERE>':
+                    blocks[seq_num]['errors'].append('Missing Translation')                                   
                 else:
                     blocks[seq_num]['errors'].append('Missing Target Sentence')
-                    # print (line)
-                    # print (max(line))
-                    # print (hex(ord(max(line))))
-                    # print (is_Burmese(line))
+
                 state = 3
             else:
                 if is_comment(line):
                     blocks[seq_num]['comments'].append(line)
 
                 elif is_blank(line):
-                    if len(blocks[seq_num]['source']) > 0 and len(blocks[seq_num]['source']) > 0 and not is_review(blocks[seq_num]['comments']):
+                    if len(blocks[seq_num]['errors']) == 0 and not is_review(blocks[seq_num]['comments']):
                         blocks[seq_num]['review'] = False
                     state = 0
                 else:
                     orphans[line_num] = line
-        else:
+        else:   # Check the last sentence in a file
             if state == 3:
-                if len(blocks[seq_num]['source']) > 0 and len(blocks[seq_num]['source']) > 0 and not is_review(blocks[seq_num]['comments']):
+                if len(blocks[seq_num]['errors']) == 0 and not is_review(blocks[seq_num]['comments']):
                         blocks[seq_num]['review'] = False
 
     return blocks, orphans
-    #print ('%d entries loaded to the dictionary' % len(my_dict.keys()))
-    #return my_dict, file_stats
 
 
-def get_review_list(context_size=2):
+def summarize_errors(blocks, orphans):
 
-    review_list = {}
+    reviews = 0
+    errors = 0
+    missing = 0
+
+    # Print only the blocks marked for review  
+    seqs = blocks.keys()
+    min_seq = min(seqs)
+
+    for b in range(min(seqs), max(seqs)):
     
-    for fname in os.listdir(trans_dir):
-        print ('Processing', fname)   
+        if not b in blocks:
+            print('Missing block:', b)
+            missing += 1
+            continue
 
-        state = 0   # Going through each line
-        context = []
-        max_context_size = context_size * 2 + 1
+        display=False
+        if blocks[b]['review']:
+            reviews += 1
+            display = True
 
-        with open(os.path.join(trans_dir, fname), encoding='utf-8') as infile:            
-            for line in infile.readlines():
-                line = line.strip()
+        if len(blocks[b]['errors']) > 0:
+            errors += 1
+            display = True
 
-                context.append(line)
-                if len(context) > max_context_size:
-                    context.pop(0)
+        if display:
+            print(b)
+            #print(yaml.dump(blocks[b], encoding='utf-8'))
+            print(blocks[b])
+            print('')
 
-                if re.match('^#.*REVIEW.*$', line):     # Found a review tag
-                    state = 1
-                    next_lines = context_size
-                elif state == 1:    # Adding context for a recently found review tag
-                    next_lines = next_lines - 1
-                    if next_lines == 0:
-                        review_list[fname] = review_list.get(fname,[])
-                        review_list[fname].append(context)
-                        state = 0
-    
-    return review_list
+    for o in orphans:
+        print(orphans[o]) 
+        errors += 1
 
 
 def write_dataset(my_dict):
@@ -231,20 +244,15 @@ def write_dataset(my_dict):
 if __name__ == '__main__':
 
     if len(sys.argv) == 1:
-    # print(get_review_list())
         mydict, stats = build_dict()
         print(stats)
         write_dataset(mydict)
     else:
+ 
         for fname in sys.argv[1:]:
             blocks, orphans = analyze_file(fname)
-            # Print only the blocks marked for review            
-            [ print(b, blocks[b]) for b in blocks if blocks[b]['review'] ]
-            [ print(orphans[o]) for o in orphans ]
+            summarize_errors(blocks, orphans)
+           
 
-            seqs = blocks.keys()
-            min_seq = min(seqs)
-            for seq in range(min(seqs), max(seqs)):
-                if not seq in seqs:
-                    print(seq, ' missing')
-            print(len(seqs))
+        
+            
